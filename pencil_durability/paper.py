6570433b69d00dd63204ddcec5c1d0640bebc230
Nicole Arruda <64596084+nicole-arruda@users.noreply.github.com>
2023-05-10 14:18:12 -0400

    Paper can write. Starting on erase.

--- pencil_durability/paper.py
+++ pencil_durability/paper.py
@@ -5,3 +5,10 @@ class Paper:
 
     def contents(self):
         return self._contents
+
+
+    def write(self, new_text):
+        self._contents += new_text
+
+    def erase(self, text_to_erase):
+        self._contents = ""
--- pencil_durability_tests/test_paper.py
+++ pencil_durability_tests/test_paper.py
@@ -4,3 +4,27 @@ from pencil_durability.paper import Paper
 def test_new_paper_is_blank():
     paper = Paper()
     assert paper.contents() == ""
+
+def test_write_hello_makes_paper_contents_hello():
+    paper = Paper()
+    paper.write("Hello")
+    assert paper.contents() == "Hello"
+
+def test_write_hello_then_how_are_you():
+    paper = Paper()
+    paper.write("Hello")
+    paper.write(" how are you")
+    assert paper.contents() == "Hello how are you"
+
+def test_write_new_line():
+    paper = Paper()
+    paper.write("Hello")
+    paper.write("\n")
+    paper.write("how are you")
+    assert paper.contents() == "Hello\nhow are you"
+
+def test_erase_hello_from_hello():
+    paper = Paper()
+    paper.write("Hello")
+    paper.erase("Hello")
+    assert paper.contents() == ""
